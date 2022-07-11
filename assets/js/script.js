36c7c3d5a5df88316ea8d576ec2b234d57c34d44
jcuetos97 <jcuetos97@gmail.com>
2022-07-11 11:07:06 -0500

    script.js update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,9 @@
 
 var container = document.getElementById("container");
-var questioncontainer = document.querySelector("questioncontainer");
 var startquiz = document.getElementById("start");
 var count = document.getElementById("counter");
-var home = document.getElementById("home");
 
-var wrong = document.getElementById("wrong");
+var wrong = document.getElementsByClassName("wrong");
 var right = document.getElementsByClassName("right");
 
 var q1 = document.getElementById("questions1");
@@ -14,38 +12,61 @@ var q3 = document.getElementById("questions3");
 var q4 = document.getElementById("questions4");
 var q5 = document.getElementById("questions5");
 
+var questions = [q1,q2,q3,q4,q5];
+
 var secondsLeft = 75;
 
-
 startquiz.addEventListener("click", setTime);
-
 
 function setTime() {
    container.style.display = "none";
    setQuestions();
    var timerInterval = setInterval(function() {
-       secondsLeft--;
+      secondsLeft--;
       count.textContent = "Time: " + secondsLeft;
-       if(secondsLeft === 0) {
-         clearInterval(timerInterval);
-       }
-     }, 1000);  
-   };
+       if(secondsLeft <= 0) {
+        for (var i = 0; i < questions.length; i++) {
+          questions[i].style.display = "none";
+          showResults();
+         }
+         clearInterval(timerInterval);  
+      }
+      
+      }, 1000); 
+  }
+  
 
-   function setQuestions() {
-      q1.style.display = "flex";
-      console.log(wrong);
-      wrong.onclick =  function () {
-      console.log ("wrong");
+  function setQuestions() {
+    q1.style.display = "flex"; 
+    value =  -1; 
+
+    for (var i = 0; i < wrong.length; i++) {
+      wrong[i].addEventListener("click", function(){
       secondsLeft -=10;
+      value +=1;
+      nextQuestion(value);
+      });
     }
 
-    right.onclick =  function () {
-      console.log ("right");
-      right.style.backgroundColor = "green";
+    for (var i = 0; i < right.length; i++) {
+      right[i].addEventListener("click", function(){
+      value  +=1;
+      nextQuestion(value);
+      });
     }
     
-   }; 
+    function nextQuestion(value) {
 
-  
+      if (value+1<questions.length){
+        questions[value].style.display = "none";
+        questions[value+1].style.display = "flex";
+      } else {
+        questions[value].style.display = "none";
+        showResults();
+      }
+    }
+  }
    
+  function showResults {
+    
+  }
